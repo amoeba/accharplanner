dd6b62866793347fde9b919c0817bec9717ecae8
Bryce Mecum <petridish@gmail.com>
2019-03-08 22:24:03 -0900

    Bring back the ampersand

--- src/Planner.vue
+++ src/Planner.vue
@@ -47,7 +47,7 @@
         </table>
       </div>
       <div class="header">
-        <h3>XP &nbsp; Luminance</h3>
+        <h3>XP &amp; Luminance</h3>
         <table>
           <tbody>
             <tr>
