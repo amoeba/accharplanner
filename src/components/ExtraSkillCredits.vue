2a5858d38a21f5e56f3d00ebb2c9c58c21993cc6
Bryce Mecum <petridish@gmail.com>
2020-08-22 12:34:17 -0800

    Fixes #364

--- src/components/ExtraSkillCredits.vue
+++ src/components/ExtraSkillCredits.vue
@@ -5,7 +5,7 @@
         <h3>Extra Skill Credits</h3>
       </div>
       <div class="header-items alt-onecol" v-if="collapsed">
-        <ExtraSkillCredit id="railrea" name="Aun Railrea" />
+        <ExtraSkillCredit id="railrea" name="Aun Ralirea" />
         <ExtraSkillCredit id="oswald" name="Chasing Oswald" />
         <ExtraSkillCredit id="luminance1" name="Luminance 1" />
         <ExtraSkillCredit id="luminance2" name="Luminance 2" />
