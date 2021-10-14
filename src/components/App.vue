17dfc214a95a0a12e73a6e3cdfbdb6d0c87b4bae
Bryce Mecum <petridish@gmail.com>
2021-10-14 00:59:04 -0800

    Fix loading of builds

--- src/components/App.vue
+++ src/components/App.vue
@@ -25,13 +25,17 @@
       <button
         aria-label="Toggle theme between dark and light"
         class="theme-toggle"
-        @click="toggleDarkMode"></button>
+        @click="toggleDarkMode"
+      ></button>
     </header>
     <main>
       <router-view></router-view>
     </main>
     <footer>
-      <div>This website created with Malar Zhapaj by <a href="https://github.com/amoeba">Kolth</a></div>
+      <div>
+        This website created with Malar Zhapaj by
+        <a href="https://github.com/amoeba">Kolth</a>
+      </div>
       <div>Icons from <a href="https://heroicons.dev/">Hero Icons</a></div>
     </footer>
   </div>
@@ -73,6 +77,83 @@ export default {
 
       this.$store.commit("toggleDarkMode", preference);
     }
+  },
+  created() {
+    // Clear any notifications stored in localStorage
+    this.$store.commit("clearAllNotifications");
+
+    // Decide if we should do things
+    const params = new URL(document.location.href).searchParams;
+
+    if (
+      !params.get("action") ||
+      !params.get("server") ||
+      !params.get("character")
+    ) {
+      return;
+    }
+
+    if (params.get("action") != "import") {
+      return;
+    }
+
+    // Reset our character before import
+    this.$store.commit("reset");
+
+    // Start doing things
+    var store = this.$store;
+    const url =
+      "https://treestats.net/" +
+      params.get("server") +
+      "/" +
+      params.get("character") +
+      ".json";
+    let errorEncountered = false;
+
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        if (!json.attribs) {
+          throw "Invalid JSON structure. Is this character a stub character (i.e., no attributes, skills)?";
+        }
+
+        return json;
+      })
+      .then(json => {
+        importCharacter(store, json);
+      })
+      .catch(error => {
+        errorEncountered = true;
+
+        store.commit("addNotification", {
+          type: "error",
+          message:
+            "Failed to import " +
+            params.get("character") +
+            " (" +
+            params.get("server") +
+            "): " +
+            error
+        });
+
+        return;
+      })
+      .then(() => {
+        if (errorEncountered) {
+          return;
+        }
+
+        store.commit("addNotification", {
+          type: "success",
+          message:
+            params.get("character") +
+            " (" +
+            params.get("server") +
+            ") successfully imported."
+        });
+      });
   }
 };
 </script>
