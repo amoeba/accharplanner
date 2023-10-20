1119aa5ac6ea33a9e47f5e216f9fb19638bebb28
Bryce Mecum <petridish@gmail.com>
2023-10-19 17:53:14 -0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ["@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt", '@nuxtjs/supabase'],
   css: ['~/assets/css/main.css'],
   postcss: {
