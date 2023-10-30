8d2a99959415d5dee76b4e29e93ec042c2b0d82f
Bryce Mecum <petridish@gmail.com>
2023-10-29 19:25:38 -0800

    Add floating-vue to nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["@pinia/nuxt", '@nuxtjs/supabase'],
+  modules: ["@pinia/nuxt", '@nuxtjs/supabase', 'floating-vue/nuxt'],
   runtimeConfig: {
     public: {
       baseUrl: 'https://planner.treestats.net'
