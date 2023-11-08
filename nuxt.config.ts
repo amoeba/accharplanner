2a0b2d9cc5330842e89c3e05d862d375668f6cfd
Bryce Mecum <petridish@gmail.com>
2023-11-07 19:48:36 -0900

    Set up googlefonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["@pinia/nuxt", '@nuxtjs/supabase', 'floating-vue/nuxt'],
+  modules: ["@pinia/nuxt", '@nuxtjs/supabase', 'floating-vue/nuxt', '@nuxtjs/google-fonts'],
   runtimeConfig: {
     public: {
       baseUrl: 'https://planner.treestats.net'
+    }
+  },
+  googleFonts: {
+    families: {
+      'League+Spartan': true,
+      Roboto: true,
+      'Josefin+Sans': true,
+      Lato: [100, 300],
+      Raleway: {
+        wght: [100, 400],
+        ital: [100]
+      },
     }
   },
   css: ['~/assets/css/main.css'],
