03d813252920114d2eb50b86fada96a7524fd29c
Bryce Mecum <petridish@gmail.com>
2023-11-08 21:14:31 -0900

    Try out less spacious buttons

--- components/Button.vue
+++ components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="flex gap-2 rounded border border-zinc-200 hover:bg-zinc-50 p-2">
+  <button class="flex gap-2 rounded border border-zinc-200 hover:bg-zinc-50 px-2 py-1">
     <slot></slot>
   </button>
 </template>
