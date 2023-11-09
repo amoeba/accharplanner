f1859a3166a9ddc4ca48e4262f906bd0348a374e
Bryce Mecum <petridish@gmail.com>
2023-11-08 21:16:16 -0900

    Bring font size down in planner

--- components/Planner/Planner.vue
+++ components/Planner/Planner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-2">
+  <div class="planner flex flex-col gap-2">
     <div class="grid grid-flow-col">
       <div class="flex gap-2">
         <Button @click="expandAll">
