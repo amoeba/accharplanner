c6af320ec1f1b5c91dd6c7c4ebf3750a27a389d5
Bryce Mecum <petridish@gmail.com>
2023-11-11 00:11:46 -0900

    Fix minor rendering bug in Pane

--- components/Pane.vue
+++ components/Pane.vue
@@ -7,7 +7,7 @@ defineProps([
 <template>
   <div class="border rounded border-zinc-200 divide-y">
     <div class="flex rounded-tl rounded-tr hover:bg-zinc-50 cursor-pointer" @click="toggleExpanded">
-      <div class="flex grow font-bold gap-2 px-2 py-1">
+      <div class="flex items-center grow font-bold gap-2 px-2 py-1">
         <slot name="title"></slot>
       </div>
       <div class="justify-self-end text-right px-2 py-1">
