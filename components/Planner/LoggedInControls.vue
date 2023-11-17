2d4852713ffa8d35dcda3f67be581222fb79b22a
Bryce Mecum <petridish@gmail.com>
2023-11-16 21:46:33 -0900

    Implement skeleton for favoriting builds

--- components/Planner/LoggedInControls.vue
+++ components/Planner/LoggedInControls.vue
@@ -7,20 +7,45 @@ const client = useSupabaseClient();
 const store = usePlannerStore();
 
 const favoriteBuild = async function () {
-  console.log("favoriteBuild")
+  if (!user) {
+    return;
+  }
 
-  // const { data, error } = await client
-  //   .from("builds")
-  //   .insert({ id: createId(10), content: store.build, is_published: true, user_id: user.value?.id })
-  //   .select();
+  // TODO: make dynamic (get it from the store)
+  const buildId = "hn7HnjGcjn";
 
-  // if (data) {
-  //   console.log(data);
-  // }
+  // Check if we've already favorited
+  const { data: selectData, error: selectError } = await client
+    .from("builds_favorites")
+    .select()
+    .eq("build_id", buildId)
+    .eq("favorited_by", user.value.id);
 
-  // if (error) {
-  //   console.log(error);
-  // }
+  if (selectError) {
+    console.log("Failed to find existing favs", selectError);
+
+    return;
+  }
+
+  if (selectData.length > 0) {
+    console.log("Fav already existed, not faving again")
+
+    return;
+  }
+
+  // Only now do we favorite
+  const { data, error } = await client
+    .from("builds_favorites")
+    .insert({ build_id: buildId })
+    .select();
+
+  if (error) {
+    console.log("Failed to update favs count", error)
+  } else {
+    // TODO: Do something more fun with succcess
+    // like a spinning star
+    console.log("Favorited successfully", data)
+  }
 }
 
 const publishBuild = async function () {
