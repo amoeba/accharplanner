6909e496bc1ace9d1a94b3dc6fe34f4fd020c2a0
Bryce Mecum <petridish@gmail.com>
2023-11-16 19:57:41 -0900

    Tweak front page

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,17 @@
   <div class="text-xl font-bold py-3">
     Welcome to ODACCP!
   </div>
+
+  <p>
+    Blurb about the Planner
+  </p>
+
+  <div class="flex justify-center">
+    <NuxtLink class="border rounded border-zinc-200 hover:bg-zinc-50 px-2 py-1" to="/planner">
+      Create a New Build
+    </NuxtLink>
+  </div>
+
   <Features class="py-3"></Features>
 
   <PublishedBuilds />
