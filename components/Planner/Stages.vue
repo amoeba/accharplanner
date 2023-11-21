8bc545d7e9b4a93d37b0e0f9997d3979fbc973b7
Bryce Mecum <petridish@gmail.com>
2023-11-20 21:37:01 -0900

    WIP Stages.vue refactor into Composition API

--- components/Planner/Stages.vue
+++ components/Planner/Stages.vue
@@ -1,95 +1,100 @@
 <template>
-  <Pane :toggleExpanded="toggleExpanded" :isExpanded="isExpanded">
+  <Pane :toggleExpanded="toggleExpanded" :isExpanded="store.ui.paneVisibility.buildStages">
     <template #title>Build Stages</template>
     <template #right>
       <Button class="px-1 py-1 text-xs" @click="save">Save Stage</Button>
     </template>
     <template #content>
       <div class="flex" v-bind:droppable="true" v-on:drop="drop" v-on:dragover="dragover">
-        <div v-if="stages.length === 0">
+        <div v-if="store.build.stages.length === 0">
           No stages have been set up for this build.
         </div>
-        <Stage v-for="(stage, index) in stages" v-bind:key="index" v-bind:index="index" v-bind:level="stage.level"
-          v-bind:data-index="index" v-bind:stages="stages.length" v-on:dragstart="dragStart" :draggable="true" />
+        <Stage v-for="(stage, index) in store.build.stages" v-bind:key="index" v-bind:index="index"
+          v-bind:level="stage.level" v-bind:data-index="index" v-bind:stages="store.build.stages.length"
+          v-on:dragstart="dragStart" :draggable="true" :isDragInprogress="isDragging" />
       </div>
     </template>
   </Pane>
 </template>
 
-<script>
+<script setup lang="ts">
 import Stage from "./Stage.vue";
 import { usePlannerStore } from "~/stores/planner";
 
-export default {
-  name: "Stages",
-  components: {
-    Stage,
-  },
-  setup() {
-    const store = usePlannerStore();
+const store = usePlannerStore();
+const isDragging = ref(false)
 
-    return {
-      store
+
+const save = async function (event: MouseEvent) {
+  event.stopPropagation();
+
+  store.saveStage();
+}
+
+const toggleExpanded = async function () {
+  store.toggleBuildStagesPane();
+}
+
+const dragStart = async function (event: DragEvent) {
+  console.log("dragStart")
+
+  if (!event || !event.dataTransfer || !event.target) {
+    return;
+  }
+
+  if (event.target.dataset) {
+    event.dataTransfer.setData("text/plain", event.target.dataset.index);
+  }
+
+  isDragging.value = true;
+}
+
+const processDrop = function (droppedX: Number, data: DataTransfer) {
+  const index = Number(data.getData("text/plain"));
+
+  console.log("drag", { droppedX: droppedX, index: index })
+
+  // TODO: Finish me
+  return;
+
+  let stages = [
+    {
+      index: index,
+      x: droppedX,
+    },
+  ];
+
+  document.querySelectorAll("div.stage").forEach((el, i) => {
+    if (i === index) {
+      return;
     }
-  },
-  data() {
-    return {
-      isDragging: false,
-    };
-  },
-  computed: {
-    stages() {
-      return this.store.build.stages;
-    },
-    isExpanded() {
-      return this.store.buildStagesPaneVisible;
-    },
-  },
-  methods: {
-    save(event) {
-      event.stopPropagation();
-      this.store.saveStage();
-    },
-    toggleExpanded() {
-      this.store.toggleBuildStagesPane();
-    },
-    dragStart(event) {
-      event.dataTransfer.setData("text/plain", event.target.dataset.index);
-      this.isDragging = true;
-    },
-    drop(event) {
-      this.isDragging = false;
 
-      const droppedX = event.clientX;
-      const index = Number(event.dataTransfer.getData("text/plain"));
+    stages.push({
+      index: i,
+      x: el.getBoundingClientRect().x,
+    });
+  });
 
-      let stages = [
-        {
-          index: index,
-          x: droppedX,
-        },
-      ];
+  stages.sort((a, b) => {
+    return a.x - b.x;
+  });
 
-      document.querySelectorAll("div.stage").forEach((el, i) => {
-        if (i === index) {
-          return;
-        }
+  store.reorderStages(stages);
+}
+const drop = async function (event: DragEvent) {
+  console.log("drag")
 
-        stages.push({
-          index: i,
-          x: el.getBoundingClientRect().x,
-        });
-      });
+  isDragging.value = false;
+  const droppedX = event.clientX;
 
-      stages.sort((a, b) => {
-        return a.x - b.x;
-      });
+  if (!event.dataTransfer) {
+    return;
+  }
 
-      this.store.reorderStages(stages);
-    },
-    dragover(event) {
-      event.preventDefault();
-    },
-  },
-};
+  processDrop(droppedX, event.dataTransfer);
+}
+
+const dragover = async function (event: DragEvent) {
+  event.preventDefault();
+}
 </script>
