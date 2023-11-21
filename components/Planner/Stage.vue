b111b8bb123b02b4b0b0449e72c520ad6292ed39
Bryce Mecum <petridish@gmail.com>
2023-11-20 21:07:12 -0900

    Swap out Lucide close icon

--- components/Planner/Stage.vue
+++ components/Planner/Stage.vue
@@ -6,9 +6,10 @@
         Level {{ level }}
       </button>
       <button class="rounded-r hover:text-red-600" @click="remove">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-          <path
-            d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x">
+          <path d="M18 6 6 18" />
+          <path d="m6 6 12 12" />
         </svg>
       </button>
     </div>
