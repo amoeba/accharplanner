70bedff6ab9cd53356b1b039fb7a8ff5be0ae1c8
Bryce Mecum <petridish@gmail.com>
2023-11-20 21:05:42 -0900

    Use less black in Stage.vue

--- components/Planner/Stage.vue
+++ components/Planner/Stage.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="flex">
-    <div class="flex border border-zinc-200 rounded divide-x p-0"
-      :class="isSelected ? 'bg-black text-white' : 'bg-transparent'">
-      <button v-bind:class="{ selected: isSelected }" class="rounded-l px-3 hover:bg-black hover:text-white cursor-grab"
+    <div class="flex border border-zinc-200 rounded divide-x p-0" :class="isSelected ? 'bg-zinc-200' : 'bg-transparent'">
+      <button v-bind:class="{ selected: isSelected }" class="rounded-l px-3 hover:bg-zinc-200 cursor-grab"
         v-on:click="load">
         Level {{ level }}
       </button>
-      <button class="rounded-r bg-black text-white hover:bg-red-600" @click="remove">
+      <button class="rounded-r hover:text-red-600" @click="remove">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
           <path
             d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
