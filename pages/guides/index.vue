d47cb3047042eb109ae60c1bf057b3bb0a3c8d08
Bryce Mecum <petridish@gmail.com>
2023-11-24 22:41:31 -0900

    prettier

--- pages/guides/index.vue
+++ pages/guides/index.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <h2 class="text-lg font-bold">Guides</h2>
-    <p class="py-2">Blurb about Guides...</p>
-    <NuxtLink href="/guides/new"> Create a new Guide </NuxtLink>
+    <h2 class="text-lg font-bold">
+      Guides
+    </h2>
+    <p class="py-2">
+      Blurb about Guides...
+    </p>
+    <ButtonView>
+      <NuxtLink href="/guides/new">
+        Create a new Guide
+      </NuxtLink>
+    </ButtonView>
     <Suspense>
       <GuideList />
-      <template #fallback> Loading... </template>
+      <template #fallback>
+        Loading...
+      </template>
     </Suspense>
   </div>
 </template>
