bb5fdd6e773fd6a76ece5ddc7d2044fb2dc57dc7
Bryce Mecum <petridish@gmail.com>
2023-11-26 00:01:37 -0900

    Increase spacing on guide form

--- components/Guide/Form.vue
+++ components/Guide/Form.vue
@@ -10,6 +10,8 @@ const props = defineProps(["submit"])
     <div class="flex flex-col">
       <label for="title">Title</label>
       <input id="title" v-model="store.guide.title" class="w-64" type="text">
+    </div>
+    <div class="flex flex-col">
       <label for="attribution">
         Attribution
         <span class="text-zinc-500">Optional</span>
@@ -17,7 +19,7 @@ const props = defineProps(["submit"])
       <input id="attribution" v-model="store.guide.attribution" type="text">
     </div>
     <div class="flex flex-col">
-      <label for="content" class="font-bold">Content</label>
+      <label for="content">Content</label>
       <GuideContentEditor class="border rounded border-black" v-model="store.guide.content" />
     </div>
     <input class="flex items-center gap-2 rounded border border-zinc-200 hover:bg-zinc-50 px-2 py-1 cursor-pointer w-auto"
