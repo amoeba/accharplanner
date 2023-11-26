55b9a536c7389d99c816e699283e45fd1144ebc8
Bryce Mecum <petridish@gmail.com>
2023-11-26 00:38:07 -0900

    Fiddle with button styling

--- components/ButtonView.vue
+++ components/ButtonView.vue
@@ -3,10 +3,8 @@ const props = defineProps(["tooltip"]);
 </script>
 
 <template>
-  <button
-    v-tooltip="props.tooltip"
-    class="flex items-center gap-2 rounded border border-zinc-200 hover:bg-zinc-50 px-2 py-1 cursor-pointer"
-  >
+  <button v-tooltip="props.tooltip"
+    class="flex items-center gap-2 rounded border bg-gray-100 border-gray-300 hover:bg-gray-200 px-2 py-1 cursor-pointer">
     <slot />
   </button>
 </template>
