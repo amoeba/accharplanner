d60f85f5900026c1e6016292dc99b1de4e746ba7
Bryce Mecum <petridish@gmail.com>
2023-11-26 00:40:07 -0900

    WIP add icons to back buttons

--- pages/guides/[id].vue
+++ pages/guides/[id].vue
@@ -8,7 +8,15 @@ const route = useRoute();
   <div class="flex flex-col gap-2">
     <div>
       <ButtonView>
-        <NuxtLink href="/guides"> Back to Guides </NuxtLink>
+        <NuxtLink href="/guides">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+            class="lucide lucide-arrow-left">
+            <path d="m12 19-7-7 7-7" />
+            <path d="M19 12H5" />
+          </svg>
+          Back to Guides
+        </NuxtLink>
       </ButtonView>
     </div>
     <Suspense>
--- pages/guides/new.vue
+++ pages/guides/new.vue
@@ -5,7 +5,15 @@ const user = useSupabaseUser();
 <template>
   <div>
     <ButtonView>
-      <NuxtLink href="/guides"> Back to Guides </NuxtLink>
+      <NuxtLink href="/guides">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+          class="lucide lucide-arrow-left">
+          <path d="m12 19-7-7 7-7" />
+          <path d="M19 12H5" />
+        </svg>
+        Back to Guides
+      </NuxtLink>
     </ButtonView>
     <div v-if="!user">
       You must be logged in to create a new Guide.
