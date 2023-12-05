30830b5f299a524209f28a016cba61934345e45e
Bryce Mecum <petridish@gmail.com>
2023-12-04 22:17:39 -0900

    Fix dark mode styling of TableWithName

--- components/TableWithName.vue
+++ components/TableWithName.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="inline-block m-2 rounded border border-zinc-200 dark:bg-stone-800 bg-white">
+  <div class="inline-block m-2 rounded border bg-white dark:bg-stone-800 border-zinc-200 dark:border-stone-600">
     <div class="rounded bg-zinc-50 dark:bg-stone-900 font-bold px-2 py-1 ">
       <slot name="title" />
     </div>
