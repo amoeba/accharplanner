4667f45fe001a47482b7dbb66bd7a7f2da5b5c9d
Bryce Mecum <petridish@gmail.com>
2023-12-06 22:09:47 -0900

    Tweak layout of guides list page

--- components/ButtonLink.vue
+++ components/ButtonLink.vue
@@ -3,10 +3,8 @@ const props = defineProps(["href"])
 </script>
 
 <template>
-  <NuxtLink
-    :href="props.href"
-    class="flex items-center gap-2 rounded border bg-gray-100 dark:bg-stone-800 border-gray-300 dark:border-stone-600 hover:bg-gray-200 dark:hover:bg-stone-700 px-2 py-1"
-  >
+  <NuxtLink :href="props.href"
+    class="flex inline-flex items-center gap-2 rounded border bg-gray-100 dark:bg-stone-800 border-gray-300 dark:border-stone-600 hover:bg-gray-200 dark:hover:bg-stone-700 px-2 py-1">
     <slot />
   </NuxtLink>
 </template>
--- pages/guides/index.vue
+++ pages/guides/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
+  <div class="flex flex-col gap-2">
     <h2 class="text-xl font-bold">
       Guides
     </h2>
-    <p class="py-2">
+    <p class="leading-6">
       Blurb about Guides...
     </p>
-    <ButtonView>
-      <NuxtLink href="/guides/new">
-        Create a new Guide
-      </NuxtLink>
-    </ButtonView>
+    <div>
+      <ButtonLink href="/guides/new">
+        Create a New Guide
+      </ButtonLink>
+    </div>
     <Suspense>
       <GuideList />
       <template #fallback>
