290c19dce41213b0043abb4aa1a78046924f06fd
Bryce Mecum <petridish@gmail.com>
2023-12-10 01:58:47 -0900

    Re-do localbuilds one more time

--- components/LocalBuildList.vue
+++ components/LocalBuildList.vue
@@ -41,13 +41,20 @@ const clear = async function () {
     </p>
     <div v-if="builds && builds.length > 0">
       <ButtonView @click="clear">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+          class="lucide lucide-trash-2">
+          <path d="M3 6h18" />
+          <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+          <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+          <line x1="10" x2="10" y1="11" y2="17" />
+          <line x1="14" x2="14" y1="11" y2="17" />
+        </svg>
         Delete All
       </ButtonView>
     </div>
-    <ul v-if="builds && builds.length > 0">
-      <li v-for="build in builds" :key="build.key">
-        <LocalBuildListItem :build="build" />
-      </li>
-    </ul>
+    <table v-if="builds && builds.length > 0" class="w-auto">
+      <LocalBuildListItem v-for="build in builds" :key="build.key" :build="build" />
+    </table>
   </div>
 </template>
