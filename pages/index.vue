7b69ce93cd48ed251f467f64115f69f5111aec55
Bryce Mecum <petridish@gmail.com>
2023-12-10 10:43:31 -0900

    Move published+latest to top of index page

--- pages/index.vue
+++ pages/index.vue
@@ -6,58 +6,34 @@
       <span>Welcome to ODACCP!</span>
     </div>
 
+    <div>
+      <Suspense>
+        <PublishedBuilds :max="5" />
+        <template #fallback>
+          Loading...
+        </template>
+      </Suspense>
+      <Suspense>
+        <LatestBuilds :max="5" />
+        <template #fallback>
+          Loading...
+        </template>
+      </Suspense>
+    </div>
+
     <div class="flex justify-center">
       <ButtonLink to="/planner">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-swords"
-        >
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+          class="lucide lucide-swords">
           <polyline points="14.5 17.5 3 6 3 3 6 3 17.5 14.5" />
-          <line
-            x1="13"
-            x2="19"
-            y1="19"
-            y2="13"
-          />
-          <line
-            x1="16"
-            x2="20"
-            y1="16"
-            y2="20"
-          />
-          <line
-            x1="19"
-            x2="21"
-            y1="21"
-            y2="19"
-          />
+          <line x1="13" x2="19" y1="19" y2="13" />
+          <line x1="16" x2="20" y1="16" y2="20" />
+          <line x1="19" x2="21" y1="21" y2="19" />
           <polyline points="14.5 6.5 18 3 21 3 21 6 17.5 9.5" />
-          <line
-            x1="5"
-            x2="9"
-            y1="14"
-            y2="18"
-          />
-          <line
-            x1="7"
-            x2="4"
-            y1="17"
-            y2="20"
-          />
-          <line
-            x1="3"
-            x2="5"
-            y1="19"
-            y2="21"
-          />
+          <line x1="5" x2="9" y1="14" y2="18" />
+          <line x1="7" x2="4" y1="17" y2="20" />
+          <line x1="3" x2="5" y1="19" y2="21" />
         </svg>
         Enter the Planner
       </ButtonLink>
@@ -76,7 +52,7 @@
 
     <ul>
       <li>
-        See the effective of:
+        See the effect of:
         <ul>
           <li>Buffs</li>
           <li>Experience Augmentations</li>
@@ -103,20 +79,5 @@
         </NuxtLink> for your Builds
       </li>
     </ul>
-
-    <div>
-      <Suspense>
-        <PublishedBuilds />
-        <template #fallback>
-          Loading...
-        </template>
-      </Suspense>
-      <Suspense>
-        <LatestBuilds />
-        <template #fallback>
-          Loading...
-        </template>
-      </Suspense>
-    </div>
   </div>
 </template>
