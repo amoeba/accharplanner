f239f3c89eae6ff9d0c3cf8efaa774f31e2f1e77
Bryce Mecum <petridish@gmail.com>
2023-12-10 21:25:19 -0900

    Redo guides list

--- components/Guide/List.vue
+++ components/Guide/List.vue
@@ -1,84 +1,21 @@
 <script setup lang="ts">
-import type { Database } from "~/utils/database.types"
+import type { Database } from "~/utils/database.types-generated"
 
-const route = useRoute()
-const client = useSupabaseClient()
-
-const page = ref(getPage(route.query.page))
-const pageSize = 20
-
-const guides = ref<Database["public"]["Tables"]["guides"]["Row"][]>()
-const guidesErrorMessage = ref("")
-const count = ref(0)
-const countErrorMessage = ref("")
-
-const doCountGuides = async function (): Promise<number> {
-  const { count, error } = await fetchGuidesCount(client)
-
-  if (error) {
-    countErrorMessage.value = error.message
-
-    return -1
-  }
-
-  return count || 0
-}
-
-const doFetchGuides = async function (page: number): Promise<Database["public"]["Tables"]["guides"]["Row"][]> {
-  const { data, error } = await fetchGuides(client, pageSize, page, pageSize)
-
-  if (error) {
-    guidesErrorMessage.value = error.message
-
-    return []
-  }
-
-  return data
-}
-
-count.value = await doCountGuides()
-guides.value = await doFetchGuides(page.value)
-
-watch(
-  () => page.value,
-  async (next, prev) => {
-    guides.value = await doFetchGuides(next)
-  },
-)
-
-const numPages = Math.ceil(count.value / pageSize)
+const props = defineProps<{ guides: Database["public"]["Tables"]["guides"]["Row"][] }>()
 </script>
 
 <template>
-  <div v-if="guides && guides.length <= 0">
+  <div v-if="props.guides.length <= 0">
     No guides created yet. <NuxtLink href="/guides/new">
       Create one
     </NuxtLink>
   </div>
-  <div v-if="countErrorMessage">
-    {{ countErrorMessage }}
-  </div>
-  <div v-if="guidesErrorMessage">
-    {{ guidesErrorMessage }}
-  </div>
-  <div v-if="count">
-    Total: {{ count }}
-  </div>
-  <ul>
-    <li
-      v-for="guide in guides"
-      :key="guide.id"
-    >
+  <ul v-if="props.guides.length > 0">
+    <li v-for="guide in props.guides" :key="guide.id">
       <NuxtLink :href="`/guides/${guide.id}`">
         {{ guide.title }}
       </NuxtLink>
       by {{ guide.attribution || (guide.profiles ? guide.profiles.name : "Anonymous") }}
     </li>
   </ul>
-  <div class="flex gap-2">
-    <PageControls
-      v-model="page"
-      :num-pages="numPages"
-    />
-  </div>
 </template>
--- pages/guides/index.vue
+++ pages/guides/index.vue
@@ -1,3 +1,54 @@
+<script setup lang="ts">
+import type { Database } from "~/utils/database.types-generated"
+
+const route = useRoute()
+const client = useSupabaseClient()
+
+const page = ref(getPage(route.query.page))
+const pageSize = 20
+
+const guides = ref<Database["public"]["Tables"]["guides"]["Row"][]>()
+const guidesErrorMessage = ref("")
+const count = ref(0)
+const countErrorMessage = ref("")
+
+const doCountGuides = async function (): Promise<number> {
+  const { count, error } = await fetchGuidesCount(client)
+
+  if (error) {
+    countErrorMessage.value = error.message
+
+    return -1
+  }
+
+  return count || 0
+}
+
+const doFetchGuides = async function (page: number): Promise<Database["public"]["Tables"]["guides"]["Row"][]> {
+  const { data, error } = await fetchGuides(client, pageSize, page, pageSize)
+
+  if (error) {
+    guidesErrorMessage.value = error.message
+
+    return []
+  }
+
+  return data
+}
+
+count.value = await doCountGuides()
+guides.value = await doFetchGuides(page.value)
+
+watch(
+  () => page.value,
+  async (next, prev) => {
+    guides.value = await doFetchGuides(next)
+  },
+)
+
+const numPages = Math.ceil(count.value / pageSize)
+</script>
+
 <template>
   <div class="flex flex-col gap-2">
     <h2 class="text-xl font-bold">
@@ -7,15 +58,19 @@
       Blurb about Guides...
     </p>
     <div>
+      <div v-if="countErrorMessage">
+        {{ countErrorMessage }}
+      </div>
+      <div v-if="guidesErrorMessage">
+        {{ guidesErrorMessage }}
+      </div>
       <ButtonLink href="/guides/new">
         Create a New Guide
       </ButtonLink>
     </div>
-    <Suspense>
-      <GuideList />
-      <template #fallback>
-        Loading...
-      </template>
-    </Suspense>
+    <GuideList v-if="guides" :guides="guides" />
+    <div class="flex gap-2">
+      <PageControls v-model="page" :num-pages="numPages" />
+    </div>
   </div>
 </template>
