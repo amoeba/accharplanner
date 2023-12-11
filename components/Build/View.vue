7cfed221fc209079cb0b9f537536e7bcedaa2148
Bryce Mecum <petridish@gmail.com>
2023-12-10 20:10:29 -0900

    Tweak build view

--- components/Build/View.vue
+++ components/Build/View.vue
@@ -3,8 +3,23 @@ const props = defineProps(["build"])
 </script>
 
 <template>
-  <div class="flex gap-2">
-    <BuildReadOnlyAttributesAndVitals :build="props.build" />
-    <BuildReadOnlySkillsView :build="props.build" />
+  <div class="flex flex-col sm:flex-row gap-2">
+    <CollapsiblePane :is-collapsible="false" :is-expanded="true">
+      <template #title>
+        Attributes &amp; Vitals
+      </template>
+      <template #content>
+        <BuildReadOnlyAttributesAndVitals :build="props.build" />
+      </template>
+    </CollapsiblePane>
+    <CollapsiblePane :is-collapsible="false" :is-expanded="true">
+      <template #title>
+        Skills
+      </template>
+      <template #content>
+        <BuildReadOnlySkillsView :build="props.build" />
+      </template>
+    </CollapsiblePane>
+
   </div>
 </template>
