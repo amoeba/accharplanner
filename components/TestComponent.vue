8aef86baf18de28f2ed0d286fd8ea13611e09f6f
Bryce Mecum <petridish@gmail.com>
2023-12-10 23:20:00 -0900

    wow

---
+++ components/TestComponent.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps(["build_id"])
+</script>
+
+<template>
+  <div>
+    Hello from TestComponent. Build ID is {{ props.build_id }}
+  </div>
+</template>
