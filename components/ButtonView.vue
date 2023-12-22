d739da126ae2038e32465f2cbe2b3b22397c5250
Bryce Mecum <petridish@gmail.com>
2023-12-21 21:28:47 -0900

    Reduce button y padding

--- components/ButtonView.vue
+++ components/ButtonView.vue
@@ -3,10 +3,8 @@ const props = defineProps(["tooltip"])
 </script>
 
 <template>
-  <button
-    v-tooltip="props.tooltip"
-    class="flex items-center gap-2 rounded border bg-gray-100 dark:bg-stone-800 border-gray-300 dark:border-stone-600 hover:bg-gray-200 dark:hover:bg-stone-700 px-2 py-1 cursor-pointer"
-  >
+  <button v-tooltip="props.tooltip"
+    class="flex items-center gap-2 rounded border bg-gray-100 dark:bg-stone-800 border-gray-300 dark:border-stone-600 hover:bg-gray-200 dark:hover:bg-stone-700 px-2 py-0 cursor-pointer leading-6">
     <slot />
   </button>
 </template>
