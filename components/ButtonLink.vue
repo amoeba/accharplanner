156fdf8fc813ffff4daefb5b85edbda9fee0189c
Bryce Mecum <petridish@gmail.com>
2023-12-22 18:15:34 -0900

    Make ButtonLink not underlined

--- components/ButtonLink.vue
+++ components/ButtonLink.vue
@@ -5,7 +5,7 @@ const props = defineProps<{ href: string }>()
 <template>
   <NuxtLink
     :href="props.href"
-    class="flex items-center gap-2 rounded border border-gray-300 bg-gray-100 px-2 py-1 text-inherit hover:bg-gray-200 dark:border-stone-600 dark:bg-stone-800 dark:hover:bg-stone-700"
+    class="flex items-center gap-2 rounded border border-gray-300 bg-gray-100 px-2 py-1 text-inherit no-underline hover:bg-gray-200 dark:border-stone-600 dark:bg-stone-800 dark:hover:bg-stone-700"
   >
     <slot />
   </NuxtLink>
