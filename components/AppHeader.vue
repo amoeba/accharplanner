e3e9afde6d736c7f83d757ff52845ea892b54e76
Bryce Mecum <petridish@gmail.com>
2023-12-22 18:35:17 -0900

    Re-order appheader items

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -55,48 +55,21 @@ const doToggleDarkMode = async function () {
       </NuxtLink>
     </div>
     <div class="flex items-center gap-2 justify-self-end">
-      <button @click="doToggleDarkMode">
-        <svg
-          v-if="store.darkMode"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <circle
-            cx="12"
-            cy="12"
-            r="4"
-          />
-          <path d="M12 2v2" />
-          <path d="M12 20v2" />
-          <path d="m4.93 4.93 1.41 1.41" />
-          <path d="m17.66 17.66 1.41 1.41" />
-          <path d="M2 12h2" />
-          <path d="M20 12h2" />
-          <path d="m6.34 17.66-1.41 1.41" />
-          <path d="m19.07 4.93-1.41 1.41" />
-        </svg>
-        <svg
-          v-if="!store.darkMode"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
-        </svg>
-      </button>
+      <!-- Login / Account -->
+      <NuxtLink
+        v-if="!user"
+        to="/login"
+        class="hidden sm:block"
+      >
+        <span v-tooltip="'Log In to Publish your builds for others to see and to favorite others\' builds.'">Log In</span>
+      </NuxtLink>
+      <NuxtLink
+        v-if="user"
+        class="hidden sm:block"
+        to="/account"
+      >
+        Account
+      </NuxtLink>
       <NuxtLink to="irc://irc.sorcery.net/#treestats">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -169,21 +142,48 @@ const doToggleDarkMode = async function () {
           />
         </svg>
       </NuxtLink>
-      <!-- Login / Account -->
-      <NuxtLink
-        v-if="!user"
-        to="/login"
-        class="hidden sm:block"
-      >
-        <span v-tooltip="'Log In to Publish your builds for others to see and to favorite others\' builds.'">Log In</span>
-      </NuxtLink>
-      <NuxtLink
-        v-if="user"
-        class="hidden sm:block"
-        to="/account"
-      >
-        Account
-      </NuxtLink>
+      <button @click="doToggleDarkMode">
+        <svg
+          v-if="store.darkMode"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle
+            cx="12"
+            cy="12"
+            r="4"
+          />
+          <path d="M12 2v2" />
+          <path d="M12 20v2" />
+          <path d="m4.93 4.93 1.41 1.41" />
+          <path d="m17.66 17.66 1.41 1.41" />
+          <path d="M2 12h2" />
+          <path d="M20 12h2" />
+          <path d="m6.34 17.66-1.41 1.41" />
+          <path d="m19.07 4.93-1.41 1.41" />
+        </svg>
+        <svg
+          v-if="!store.darkMode"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+        </svg>
+      </button>
       <BurgerMenu class="block sm:hidden" />
     </div>
   </header>
