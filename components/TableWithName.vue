0c764d6d4645bbc6ddf3f0975501e18132f820a9
Bryce Mecum <petridish@gmail.com>
2023-12-30 09:48:05 -0900

    Fix border on tablewithname

--- components/TableWithName.vue
+++ components/TableWithName.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-2 inline-block rounded border border-zinc-200 bg-white dark:border-stone-600 dark:bg-stone-800">
-    <div class="rounded bg-zinc-50 px-2 py-1 font-bold dark:bg-stone-900 ">
+    <div class="rounded-t border-b bg-zinc-50 px-2 py-1 font-bold dark:bg-stone-900 ">
       <slot name="title" />
     </div>
     <div class="px-2 py-1">
