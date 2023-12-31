8ed28087d89f5820ab0a34fffd864e2b36f0045b
Bryce Mecum <petridish@gmail.com>
2023-12-31 10:29:20 -0900

    Remove margin from TableWithName

--- components/TableWithName.vue
+++ components/TableWithName.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="m-2 inline-block rounded border border-zinc-200 bg-white dark:border-stone-600 dark:bg-stone-800">
+  <div class="inline-block rounded border border-zinc-200 bg-white dark:border-stone-600 dark:bg-stone-800">
     <div class="rounded-t border-b bg-zinc-50 px-2 py-1 font-bold dark:bg-stone-900 ">
       <slot name="title" />
     </div>
