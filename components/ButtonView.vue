1d48a375fe4a1239ccbd5512bc63289cebed8fae
Bryce Mecum <petridish@gmail.com>
2024-01-07 10:31:29 -0900

    Give buttons a min-height

--- components/ButtonView.vue
+++ components/ButtonView.vue
@@ -3,7 +3,7 @@
 
 <template>
   <button
-    class="flex cursor-pointer items-center gap-1 rounded border border-gray-300 bg-gray-100 px-2 py-0 leading-6 hover:bg-gray-200 dark:border-stone-600 dark:bg-stone-800 dark:hover:bg-stone-700"
+    class="flex min-h-6 cursor-pointer items-center gap-1 rounded border border-gray-300 bg-gray-100 px-2 py-0 leading-6 hover:bg-gray-200 dark:border-stone-600 dark:bg-stone-800 dark:hover:bg-stone-700"
   >
     <slot />
   </button>
