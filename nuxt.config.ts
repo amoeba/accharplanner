75777ba420c95968e8dfe96ed4c86b46aae117a1
Bryce Mecum <petridish@gmail.com>
2025-12-13 15:40:09 -0800

    Add compatibilityDate to nuxt.config.ts
    I guess they want you to do this now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2025-12-13',
   devtools: { enabled: false },
   modules: [
     "@nuxtjs/supabase",
