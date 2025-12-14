05e8b8a30363bb63a8ea4dcb2be2cb396b034bc8
Bryce Mecum <petridish@gmail.com>
2025-12-13 22:11:37 -0800

    fix old base migration

--- supabase/migrations/20240101193433_remote_schema.sql
+++ supabase/migrations/20240101193433_remote_schema.sql
@@ -10,20 +10,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
-
-CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
-
-CREATE EXTENSION IF NOT EXISTS "pg_stat_statements" WITH SCHEMA "extensions";
-
-CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
-
-CREATE EXTENSION IF NOT EXISTS "pgjwt" WITH SCHEMA "extensions";
-
-CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
-
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
-
 SET default_tablespace = '';
 
 SET default_table_access_method = "heap";
