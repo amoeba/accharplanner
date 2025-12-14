ccc42cf337da3aba98f49bc17da05b0375ae93dd
Bryce Mecum <petridish@gmail.com>
2025-12-13 16:24:56 -0800

    Delete pinia-persistedstate.client.ts

--- plugins/pinia-persistedstate.client.ts
+++
@@ -1,6 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-
-export default defineNuxtPlugin(({ $pinia }) => {
-  $pinia.use(piniaPluginPersistedstate)
-})
